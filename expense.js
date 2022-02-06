87af4f3e2f9a414be95029c0ef3da984917af19e
TomNikoloff <tomnikoloff@hotmail.co.uk>
2022-02-06 07:59:08 +0000

    Initial commit

---
+++ expense.js
@@ -0,0 +1,44 @@
+
+const nameEx = document.querySelector('#name');
+const date = document.querySelector('#date');
+const amount = document.querySelector('#amount');
+const addItem = document.querySelector('#add_Item');
+const table = document.querySelector('table');
+const input = document.querySelectorAll('input');
+
+function inputLengths() {
+    return input.value.length;
+}
+
+function expenseTracker() {
+    let newTr = document.createElement('tr');
+    table.appendChild(newTr);
+
+    let nameTd = document.createElement('td');
+    nameTd.appendChild(document.createTextNode(nameEx.value));
+    newTr.appendChild(nameTd);
+
+    let dateTd = document.createElement('td');
+    dateTd.appendChild(document.createTextNode(date.value));
+    newTr.appendChild(dateTd);
+
+    let amountTd = document.createElement('td');
+    amountTd.appendChild(document.createTextNode('£'+ amount.value));
+    newTr.appendChild(amountTd);
+
+    let dTd = document.createElement('td');
+    let dBtn = document.createElement('button');
+    dBtn.appendChild(document.createTextNode('X'));
+    dTd.appendChild(dBtn);
+    newTr.appendChild(dTd);
+
+    function deleteRow() {
+        newTr.classList.add('delete');
+    }
+
+    dBtn.addEventListener('click', deleteRow);
+
+}
+
+addItem.addEventListener('click', expenseTracker);
+
